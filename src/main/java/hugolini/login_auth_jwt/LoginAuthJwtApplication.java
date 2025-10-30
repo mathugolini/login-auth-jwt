62396794e42c1d33d2891e76fe1b02c6028e0712
mathugolini <mathugolini@gmail.com>
2025-10-29 21:51:15 -0300

    dependencia jwt adicionada

---
+++ src/main/java/hugolini/login_auth_jwt/LoginAuthJwtApplication.java
@@ -0,0 +1,13 @@
+package hugolini.login_auth_jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginAuthJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginAuthJwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hugolini/login_auth_jwt/LoginAuthJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package hugolini.login_auth_jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginAuthJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
